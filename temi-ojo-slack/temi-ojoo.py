9788c5633b759957146a06fb23247e5073e9e44d
temiloluwa ojo <108639654+Dreal-Godson2311@users.noreply.github.com>
2023-09-12 17:27:45 +0100

    Add files via upload

---
+++ temi-ojo-slack/temi-ojoo.py
@@ -0,0 +1,41 @@
+import pprint
+from flask import *
+import json, time
+import datetime
+from flask import Flask, request, jsonify
+
+
+
+app = Flask(__name__)
+utc_time = datetime.datetime.utcnow()
+weekday = datetime.datetime.today().weekday()
+weekday_str = "Monday" if weekday == 0 else "Tuesday" if weekday == 1 else "Wednesday" if weekday == 2 else "Thursday" if weekday == 3 else "Friday" if weekday == 4 else "Saturday" if weekday == 5 else "Sunday"
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set ={'Page':'Home', 
+               'Message':'Successfully loaded the home page',
+               'current_day': weekday_str, 
+               'utc_time': utc_time.isoformat()}
+    
+    
+    
+    return jsonify(data_set)
+
+@app.route('/user/', methods=['GET'])
+def request_page():
+    user_query = str(request.args.get('user'))
+    data_set ={'Slack_name':f'{user_query}',
+               'current_day': weekday_str,'utc_time': utc_time.isoformat(),
+               'track':'backend',
+               "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+               "github_repo_url": "https://github.com/username/repo",
+               "status_code": 200}
+    
+    
+    
+    
+    return jsonify(data_set)
+
+if __name__ == '__main__':
+    app.run(port=2307)
